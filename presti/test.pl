3cb5db0e854df4e4ef122fae16ddfea6213dee2d
iankorf <ifkorf@gmail.com>
2022-05-03 13:15:47 -0700

    test

---
+++ presti/test.pl
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+
+my $TF = "values.txt";
+my $sum = "cksum | cut -f1 -d ' '";
+
+my @TEST = (
+
+	{
+		name => "presti",
+		cli  => "./presti $TF 1 5 20 | $sum",
+		pass => "3219087707",
+	},
+
+);
+
+my $passed = 0;
+foreach my $test (@TEST) {
+	print STDERR "$test->{name}: ";
+	my $result = `$test->{cli}`;
+	chomp($result);
+	if ($result eq $test->{pass}) {
+		$passed++;
+		print STDERR "passed\n";
+	} else {
+		print STDERR "failed\n";
+	}
+}
+
+print "passed $passed / ", scalar(@TEST), " tests\n";
